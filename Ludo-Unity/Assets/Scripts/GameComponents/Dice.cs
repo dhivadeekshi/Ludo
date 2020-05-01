5d9cb672f811a1d9f89997a7c6386a80a3b14f91
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2020-05-02 00:53:47 +0530

    Modified Dice roll to have flexiblity in no's rolled

--- Ludo-Unity/Assets/Scripts/GameComponents/Dice.cs
+++ Ludo-Unity/Assets/Scripts/GameComponents/Dice.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System;
 
 public class Dice
 {
     public static int RollDice()
     {
-        return new System.Random().Next(Constants.DiceRoll.min, Constants.DiceRoll.max + 1);
+        const int tempMultiplier = 100;
+        int tempMax = Constants.DiceRoll.max * tempMultiplier;
+        int random = new Random().Next(Constants.DiceRoll.min, tempMax + 1);
+        random = (int)Math.Ceiling((double)random / tempMultiplier);
+        return random;
     }
 }
